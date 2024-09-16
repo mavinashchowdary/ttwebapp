71542498b4c4a0e0bc19ff4bdd397199dea25400
mavinashchowdary <mavinashchowdaryofficial@gmail.com>
2024-09-16 11:30:59 +0530

    Add files via upload

---
+++ javascript_basics.js
@@ -0,0 +1,24 @@
+// push and pop
+
+// push helps to add elements at last
+
+// pop removes element from last
+
+// shift and unshift
+
+// shift helps to remove elements from start unlike pop
+
+// unshift helps to add elements at first unlike push
+
+const myArray = [1,2,3,4];
+
+const x = myArray.shift();
+
+console.log(x);
+
+console.log(myArray);
+
+myArray.unshift(1);
+
+console.log(myArray);
+
